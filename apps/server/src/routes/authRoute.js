17ece40e7d903f82bec7ea871ce83e89c1fd6e96
rizqirakun <rizqirakun@gmail.com>
2023-07-21 20:48:13 +0700

    [fix] change express with api path

--- apps/server/src/routes/authRoute.js
+++ apps/server/src/routes/authRoute.js
@@ -2,8 +2,8 @@ const { Register, Login } = require('../controllers/authController');
 const { userVerification } = require('../middlewares/authMiddleware');
 const router = require('express').Router();
 
-router.post('/', userVerification)
-router.post('/register', Register)
-router.post('/login', Login)
+router.post('/api', userVerification)
+router.post('/api/register', Register)
+router.post('/api/login', Login)
 
 module.exports = router
